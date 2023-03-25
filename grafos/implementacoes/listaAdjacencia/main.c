c7b17d4eebfb81c864e0fbfa41140c830bf1e08c
FranciscoGJR <franciscoogjr@gmail.com>
2023-03-24 21:44:38 -0300

    Adicionando Struct do Graph

--- grafos/implementacoes/listaAdjacencia/main.c
+++ grafos/implementacoes/listaAdjacencia/main.c
@@ -5,24 +5,28 @@
 #define false 0
 #define true 1
 
-	// ESTRUTURA DO NO
-typedef struct s{
-	struct s* prox;
-	int valorNo;
-} NO;
+// Estrutura de um nó da lista de adjacência
+typedef struct NODE{
+    struct NODE* next;
+    int dest;
+} NODE;
 
-	// ESTRUTURA DA LISTA
-typedef struct{
-	int flag;       // o atributo flag sera usado mais a frente para busca em profundidade
-	NO* inicio;
-} VERTICE;
+// Estrutura de uma lista de adjacência
+typedef struct AdjList {
+    NODE* head;
+} AdjList;
 
+// Estrutura do grafo
+typedef struct Graph {
+    int num_vertices;
+    AdjList* array;
+} Graph;
 
 
 void main(){
 
-	// CRIAR LISTA E PREENCHER COM NULL
-    VERTICE* grafoEmLista = (VERTICE*) malloc(sizeof(VERTICE) * 5);
+
     printf("=== FINAL DO PROGRAMA ===\n");
+
 
 }
