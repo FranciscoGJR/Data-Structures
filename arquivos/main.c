6aa7d7334139c42a3196f9f8f64354160dbd227b
FranciscoGomesJunior <85801829+FranciscoGomesJunior@users.noreply.github.com>
2023-06-05 00:17:32 +0000

    r[0.1] - apresentar conteudo de um arquivo na tela

---
+++ arquivos/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/* == TAREFA ==
+1. Leitura de um arquivo de texto:
+    - Abra um arquivo de texto existente em modo de leitura.
+    - Leia e exiba o conteúdo do arquivo na tela.
+*/
+int main() {    
+    
+}
